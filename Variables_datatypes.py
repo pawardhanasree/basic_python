6a5c93b18b9b57db07f20feaed37dcb96de105d8
DHANA SREE <pawar.dhana.sree@accenture.com>
2024-11-15 12:43:58 +0530

    Added variables, datatypes file

---
+++ Variables_datatypes.py
@@ -0,0 +1,42 @@
+#Variables and datatypes
+
+#Numeric data 
+print("Numeric Data:")
+int1 = 4
+float1 = 5.98
+complex1 = complex(1, 7)
+print(int1, float1, complex1, sep=', ')
+print("Type of int1", type(int1))
+print("Type of float1", type(float1))
+print("Type of complex1", type(complex1))
+
+#Text data
+print("Text Data:")
+str1 = "Hello!"
+str2 = 'P'
+print(str1, str2, sep=", ")
+print("Type of str1", type(str1))
+print("Type of str2", type(str2))
+
+#Boolean data 
+print("Boolean Data:")
+bool1 = True
+print(bool1)
+print("Type of bool1", type(bool1))
+
+#Sequence data
+print("Sequence Data:")
+list1 = [8,2,3,[-4,7],["apple", "Banana"]]
+tuple1 = ((5,9),("Lion", "Tiger"))
+print(list1, tuple1, sep="\n")
+print("Type of list1", type(list1))
+print("Type of tuple1", type(tuple1))
+
+#Mapped data
+print("Mapped Data:")
+dict1 = {"name":"Dhana Sree", "age":23}
+print(dict1)
+print("Type of dict1", type(dict1))
+
+
+
