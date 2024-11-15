a2780a0c95d807ad8aea7024dc6aa9b9b044d2c3
DHANA SREE <pawar.dhana.sree@accenture.com>
2024-11-15 09:41:02 +0530

    Added print, commit, escape_sequence

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # basic_python
 It contains basic python programs
+
+1.print statement, comments, escape sequence charcaters
---
+++ print_comments_and_escape.py
@@ -0,0 +1,28 @@
+#To print the characters or strings have to use either single or double quotes
+print("Hello world")
+
+#We can print numbers without quotes
+print(5)
+
+#We can print mutiple strings by using comma
+print("pawardhanasree", 19)
+
+#We can perform maths calculations 
+print(15*34)
+
+#We can add seperator, and end parameters,they are optional
+print("Hi","Everybody", sep="~", end="!\n")
+
+#Escape characters
+
+#We can write the statement in next line using \n command in the string
+print("Apple\nBanana")
+
+#Multi-line sequence
+'''If we want to print quotes("") in the output, we use \".
+To print \ in the test, we can use \\'''
+
+print("My name is \"Dhana Sree Pawar\"")
+print("C:\\Users\\pawar.dhana.sree")
+
+
