a277984233221a9364fd51871518ea68743c08a6
DHANA SREE <pawar.dhana.sree@accenture.com>
2024-11-15 14:28:19 +0530

    Added arithmetic operators

---
+++ Arithmetic_operators.py
@@ -0,0 +1,12 @@
+#Arithmetic Operations
+
+num1 = 19
+num2 = 10
+print("The two numbers are:", num1, num2)
+print("Addition =", num1 + num2)
+print("Subtraction =", num1 - num2)
+print("Multiplication =", num1 * num2)
+print("Division =", num1 / num2)
+print("Modulus =", num1 % num2)
+print("Floor division =", num1 // num2)
+print("Exponential =", num1 ** num2)
