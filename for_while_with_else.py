f4a4a5a96decc2d028df29ccd5755749b0f30132
DHANA SREE <pawar.dhana.sree@accenture.com>
2024-11-23 20:27:05 +0530

    loops with else block

---
+++ for_while_with_else.py
@@ -0,0 +1,23 @@
+#for-else block
+for i in range(5):
+    print("Number {}".format(i+1))
+else:
+    print("for Else block")
+
+#while-else block
+i = 0
+while (i < 5):
+    print(i)
+    i = i + 1
+else:
+    print("While else block")
+
+#if we use the break statement in loops
+i = 0
+while (i < 5):
+    print(i)
+    i = i + 1
+    if i == 3:
+        break
+else:
+    print("While else block")
